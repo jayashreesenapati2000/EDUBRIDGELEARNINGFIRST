4778107b87d8be833ab8bff0fd159b7ea3f36f86
DELL <DELL@DESKTOP-AJI8P15>
2022-09-03 09:43:37 +0530

    datatypes practice

---
+++ src/first/Datatypetest.java
@@ -0,0 +1,41 @@
+package first;
+
+public class Datatypetest {
+	public static void main(String[] args) {
+		//byte
+		byte num1 = 121;
+		System.out.println(num1);
+		
+		//short
+		short num2 = 101;
+		System.out.println(num2);
+		
+		//int
+		int num3 = 2569754;
+		System.out.println(num3);
+		
+		//long
+		long num4 =2025369l;
+		System.out.println(num4);
+		
+		//float
+		float num5 =2.8f;
+		System.out.println(num5);
+		
+		//double
+		double num6 =20.3365d;
+		System.out.println(num6);
+		
+		//char
+		 char a = 'J';
+		System.out.println(a);
+		
+		//boolean
+		boolean b = true;
+		System.out.println(b);
+		
+		
+	}
+	
+
+}
